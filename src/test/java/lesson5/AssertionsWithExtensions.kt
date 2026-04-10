71fc58d902e4afea4b78982c0c87a8cb6d2d9a1a
AlenaSKrash <alenasavila1@outlook.com>
2026-04-10 21:48:41 +0100

    Lesson 5

--- src/test/java/lesson5/AssertionsWithExtensions.kt
+++ src/test/java/lesson5/AssertionsWithExtensions.kt
@@ -1,27 +1,21 @@
 package lesson5
 
+import io.kotest.assertions.fail
+import io.kotest.matchers.shouldBe
+
 class AssertionsWithExtensions() {
     companion object {
-        fun String.containsDigitsWithOptionalPlus(): Boolean {
-            if (startsWith("+")) { return drop(1).all { it.isDigit() } }
-            return all { it.isDigit() }
-        }
+        fun String.containsDigitsWithOptionalPlus(): Boolean =
+            isNotEmpty() && removePrefix("+").all { it.isDigit() }
 
-        fun String.hasValidLength(): Boolean {
+        fun String.hasLengthIn10To15(): Boolean {
             val digits = filter { it.isDigit() }
             return digits.length in 10..15
         }
 
-        fun String.isValidPhoneNumber(): Boolean {
-            return containsDigitsWithOptionalPlus() &&
-                    hasValidLength()
-        }
-
         infix fun String.shouldBeValidPhoneNumber(expected: Boolean) {
-            val actual = this.isValidPhoneNumber()
-            if (actual != expected) {
-                throw AssertionError("Phone '$this': expected $expected, got $actual")
-            }
+            val actual = this.containsDigitsWithOptionalPlus() && this.hasLengthIn10To15()
+            actual shouldBe expected
         }
     }
 }
--- src/test/java/lesson5/TestAssertionsWithExtensions.kt
+++ src/test/java/lesson5/TestAssertionsWithExtensions.kt
@@ -2,7 +2,7 @@ package lesson5
 
 import io.kotest.matchers.shouldBe
 import lesson5.AssertionsWithExtensions.Companion.containsDigitsWithOptionalPlus
-import lesson5.AssertionsWithExtensions.Companion.hasValidLength
+import lesson5.AssertionsWithExtensions.Companion.hasLengthIn10To15
 import lesson5.AssertionsWithExtensions.Companion.shouldBeValidPhoneNumber
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -10,12 +10,13 @@ import org.junit.jupiter.api.Test
 class TestAssertionsWithExtensions {
 
     private val phoneNumber = "+79618954971"
+    private val invalidPhoneNumber = "+++2323232323"
 
     @Test
     @DisplayName("Валидация номера телефона")
     fun checkIfPhoneNumberIsValid() {
             phoneNumber.containsDigitsWithOptionalPlus() shouldBe true
-            phoneNumber.hasValidLength() shouldBe true
+            phoneNumber.hasLengthIn10To15() shouldBe true
     }
 
     @Test
